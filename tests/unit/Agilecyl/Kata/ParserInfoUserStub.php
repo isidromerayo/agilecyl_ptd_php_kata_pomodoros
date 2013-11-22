7417963d4cc3a6b0cf53cd0df878749dab9d97eb
Isidro Merayo <isidro.merayo@gmail.com>
2013-11-22 21:37:57 +0100

    Los tres frameworks de test dobles

--- tests/unit/Agilecyl/Kata/ParserInfoUserStub.php
+++ tests/unit/Agilecyl/Kata/ParserInfoUserStub.php
@@ -20,8 +20,32 @@ class ParserInfoUserStub extends \PHPUnit_Framework_TestCase {
         $this->assertEquals('juanperez@ejemplo.com',$user->email());
         $this->assertEquals('juan',$user->password());
     }
-    
-    
+    /** @test */
+    public function stubMockery() {
+        $fileMock = \Mockery::mock('\Agilecyl\Util\FicheroLineas');
+        $fileMock->shouldReceive('getFileContentAsArray')->once()->andReturn($this->dataOneLine());
+        $parser = new \Agilecyl\Kata\ParserInfoUser();
+        // Act
+        $data = $fileMock->getFileContentAsArray();
+        $user = $parser->rawUser($data);
+        // Assert
+        $this->assertEquals('juanperez@ejemplo.com',$user->email());
+        $this->assertEquals('juan',$user->password());
+    }
+    /** @test */
+    public function stubPhake() {
+        $fileMock = \Phake::mock('\Agilecyl\Util\FicheroLineas');
+        \Phake::when($fileMock)->getFileContentAsArray()->thenReturn($this->dataOneLine());
+        $parser = new \Agilecyl\Kata\ParserInfoUser();
+        // Act
+        $data = $fileMock->getFileContentAsArray();
+        $user = $parser->rawUser($data);
+        // Assert
+        $this->assertEquals('juanperez@ejemplo.com',$user->email());
+        $this->assertEquals('juan',$user->password());
+    }
+
+
     
     private function dataOneLine() {
         return 'juan;pérez peréz;juanperez@ejemplo.com;juan;[contabilidad,dirección]';
